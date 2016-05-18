4866be95d231e59d494d68cc1dc9b6dce5d2d44f
Ted Malmgren <tmback74@hotmail.com>
2016-05-18 22:49:41 +0200

    new stuff

---
+++ HotellSavajBooking/EditForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HotellSavajBooking
+{
+    public partial class EditForm : Form
+    {
+        private DbHandler dbCon;
+        private Booking bookingToEdit;
+        public EditForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBookingSearch_Click(object sender, EventArgs e)
+        {
+            ValidateAndGet(txbBookingNr.Text);
+        }
+
+        private void ValidateAndGet(string text)
+        {
+            if (!text.Equals(string.Empty))
+                Console.Write("test");
+                //bookingToEdit = dBCon(text);
+        }
+    }
+}
