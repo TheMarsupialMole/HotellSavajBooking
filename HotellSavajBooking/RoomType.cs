9e65eeb6c59856c38e785b5eabca1902e00cfdc3
Radovan Mojsovski <radovan_mojsovski@hotmail.com>
2016-05-18 16:19:24 +0200

    Adde classes for Room, Booking and an Enum

---
+++ HotellSavajBooking/RoomType.cs
@@ -0,0 +1,15 @@
+﻿
+namespace HotellSavajBooking
+{
+    /// <summary>
+    /// Class written by Radovan Mojsovski 
+    /// 
+    /// An enum that holds the different types of Rooms at the hotel.
+    /// </summary>
+    public enum RoomType
+    {
+        Dubbelrum,
+        Enkelrum,
+        Suite
+    }
+}
