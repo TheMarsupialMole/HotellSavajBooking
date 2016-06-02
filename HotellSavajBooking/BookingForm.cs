c485eecb4ecaf078dfd7b74781a6b37f8e722004
Radovan Mojsovski <radovan_mojsovski@hotmail.com>
2016-06-02 10:49:07 +0200

    chanfes

--- HotellSavajBooking/BookingForm.cs
+++ HotellSavajBooking/BookingForm.cs
@@ -139,7 +139,7 @@ namespace HotellSavajBooking
 
         private void button1_Click_1(object sender, EventArgs e)
         {
-            new EditForm().Show();
+            new EditForm(_dbHandler).Show();
         }
     }
 }
