a3038bae0a185c59af713c155df780836b9c6c1f
Radovan Mojsovski <radovan_mojsovski@hotmail.com>
2016-06-02 10:37:44 +0200

    small change

--- HotellSavajBooking/BookingForm.Designer.cs
+++ HotellSavajBooking/BookingForm.Designer.cs
@@ -319,6 +319,8 @@
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "BookingForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Hotel Savaj Booking";
